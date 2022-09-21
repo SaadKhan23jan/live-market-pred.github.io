df7ad3eface25a4d9006124d78b6e773902853b6
Saad Khan <saadkhan23jan@gmail.com>
2022-09-21 23:31:24 +0200

    initial commit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from functions import sarimax_pred
 
 css_sheet = [dbc.themes.SKETCHY]
 BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+
 app = Dash(__name__, external_stylesheets=css_sheet)
 server = app.server
 
